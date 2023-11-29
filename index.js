149767a0dcee61bfe8c279f469875edee65e7711
Rika4698 <rikasharmin181@gmail.com>
2023-11-29 08:41:10 +0600

    send survey data in mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const moment = require('moment');
 const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const port = process.env.PORT||5000;
@@ -28,6 +29,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const userCollection = client.db("surveyDb").collection("users");
+    const surveyCollection = client.db("surveyDb").collection("survey");
     //jwt
 
     app.post('/jwt',async(req,res)=>{
@@ -35,19 +37,88 @@ async function run() {
         console.log('user for token', user);
         const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'});
         res.send({token});
-        // res.cookie('token',token,{
-        //   httpOnly:true,
-        //   secure: process.env.NODE_ENV === 'production',
-        // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-        //   // secure:true,
-        //   // sameSite:'none'
-        // })
-        // .send({success:true});
+        
       })
-      app.get('/user',  async (req, res) => {
+
+
+     const verifyToken =(req,res,next) =>{
+        console.log('inside verify token', req.headers.authorization);
+        if(!req.headers.authorization){
+            return res.status(401).send({message: 'unauthorized access'});
+        }
+        const token = req.headers.authorization.split(' ')[1];
+        console.log(token);
+        jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded) => {
+            if(err){
+                return res.status(401).send({message:'unauthorized access'})
+            }
+            req.decoded = decoded;
+            next();
+        })
+        // next();
+     }
+
+     const verifyAdmin = async (req,res,next)=>{
+        const email = req.decoded.email;
+        const query = {email: email};
+        const user = await userCollection.findOne(query);
+        const isAdmin = user?.role ==='admin';
+        if(!isAdmin){
+            return res.status(403).send({message: 'forbidden access'});
+
+        }
+        next();
+     }
+    //  const verifySurveyor = async (req,res,next)=>{
+    //     const email = req.decoded.email;
+    //     const query = {email: email};
+    //     const user = await userCollection.findOne(query);
+    //     const isSurveyor = user?.role ==='surveyor';
+    //     if(!isSurveyor){
+    //         return res.status(403).send({message: 'forbidden access'});
+
+    //     }
+    //     next();
+    //  }
+
+
+
+      app.get('/user', verifyToken,verifyAdmin, async (req, res) => {
+        // console.log(req.headers);
         const result = await userCollection.find().toArray();
         res.send(result);
       });
+
+
+      app.get('/user/admin/:email',verifyToken,async(req,res) => {
+        const email = req.params.email;
+        if(email!==req.decoded.email){
+            return res.status(403).send({message:'forbidden access'})
+
+        }
+        const query ={email:email};
+        const user = await userCollection.findOne(query);
+        let admin = false;
+        if(user){
+            admin = user?.role === 'admin';
+        }
+        res.send({admin});
+      })
+
+      app.get('/user/surveyor/:email',verifyToken,async(req,res) => {
+        const email = req.params.email;
+        if(email!==req.decoded.email){
+            return res.status(403).send({message:'forbidden access'})
+
+        }
+        const query ={email:email};
+        const user = await userCollection.findOne(query);
+        let surveyor = false;
+        if(user){
+            surveyor = user?.role === 'surveyor';
+        }
+        res.send({surveyor});
+      })
       
      app.post('/user',async(req,res) => {
 
@@ -60,8 +131,19 @@ async function run() {
         const result = await userCollection.insertOne(user);
         res.send(result);
      })
+
+     app.post('/survey',async(req,res)=>{
+        const item = req.body;
+        const surveyData = {
+            ...item,
+            timestamp: moment().format('MMMM Do YYYY, h:mm:ss a'),
+          };
+        console.log(surveyData);
+        const result =await surveyCollection.insertOne(surveyData);
+        res.send(result);
+     })
     
-     app.patch('/user/admin/:id', async(req,res) =>{
+     app.patch('/user/admin/:id',verifyToken,verifyAdmin, async(req,res) =>{
         const id = req.params.id;
         const filter ={_id: new ObjectId(id)};
         const updatedDoc = {
@@ -72,8 +154,8 @@ async function run() {
         const result = await userCollection.updateOne(filter,updatedDoc);
         res.send(result);
      })
-     
-     app.patch('/user/surveyor/:id', async(req,res) =>{
+
+     app.patch('/user/surveyor/:id',verifyToken,verifyAdmin, async(req,res) =>{
         const id = req.params.id;
         const filter ={_id: new ObjectId(id)};
         const updatedDoc = {
@@ -87,7 +169,7 @@ async function run() {
 
 
 
-      app.delete ('/user/:id',async(req,res) =>{
+      app.delete ('/user/:id',verifyToken,verifyAdmin,async(req,res) =>{
          
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
